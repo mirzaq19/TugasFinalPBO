397ba48872bd2aca1d9af56c17c2bf4732011026
mirzaq19 <mirzaqarjap@gmail.com>
2020-12-28 23:38:39 +0700

    TapTapBall

---
+++ Final Project/TapTapBall/src/id/ac/its/MHP/Main.java
@@ -0,0 +1,21 @@
+package id.ac.its.MHP;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame obj=new JFrame();
+		Gameplay gamePlay = new Gameplay();
+		
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("Tap Tap Ball!");		
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gamePlay);
+        obj.setVisible(true);
+		
+	}
+
+}
