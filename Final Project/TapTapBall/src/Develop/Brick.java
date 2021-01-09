edb750edcb8efc6abca3bc40c4fb0e70b82ceec7
nurputrak <nurputrakhanafi@gmail.com>
2021-01-10 00:27:20 +0700

    Update Brick.java

--- Final Project/TapTapBall/src/Develop/Brick.java
+++ Final Project/TapTapBall/src/Develop/Brick.java
@@ -14,6 +14,25 @@ public class Brick extends GameObject{
 	public void draw(Graphics2D g) {
 		g.setColor(color);
 		g.fillRect(x, y, width, height);
+		
+		if(color == color.cyan) {
+			if(value == 4) {
+				g.setColor(Color.red);
+				g.fillRect(x+width/4, y+height/4, width/2, height/2);
+			}
+			else if(value == 5) {
+				g.setColor(Color.blue);
+				g.fillRect(x+width/4, y+height/4, width/2, height/2);
+			}
+			else if(value == 6 || value == 7) {
+				g.setColor(Color.green);
+				g.fillRect(x+width/4, y+height/4, width/2, height/2);
+			}
+			else if(value == 8) {
+				g.setColor(Color.black);
+				g.fillRect(x+width/4, y+height/4, width/2, height/2);
+			}
+		}
 		g.setStroke(new BasicStroke(3));
 		g.setColor(Color.black);
 		g.drawRect(x, y, width, height);
